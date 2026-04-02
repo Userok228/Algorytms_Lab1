89d23cfe12a4f0898ec4c0e869d505fa44c1f281
PolishchukD <d.h.polishchuk@student.khai.edu>
2026-04-02 10:59:51 +0300

    Додано класс редактора діаграми класів. Виправлено README

--- Lab_1/obj/Debug/net9.0/Lab_1.AssemblyInfo.cs
+++ Lab_1/obj/Debug/net9.0/Lab_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aed71021bf1c5d36a45f335656ec505bde2b609")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dfeb7b158d04d9a6d756ed4c1321eb130bea54b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
